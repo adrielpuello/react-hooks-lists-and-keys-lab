ecaa3444075de56d08d6f5ed3b826ef05e483400
Adriel Puello <adrielpuello@gmail.com>
2022-09-21 14:42:47 -0400

    commit tree 2aa4836d2ae3ab10239a8f8cc4f5a8a15f6db2fe

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-function ProjectItem({ name, about, technologies }) {
+function ProjectItem({ name, about, technologies, isObject }) {
+  
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        <span>{technologies[0]}</span>
+        <span>{technologies[1]}</span>
+        {isObject ? <span>{technologies[2]}</span> : null}
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,36 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
-function ProjectList({ projects }) {
-  console.log(projects);
+const projects = [
+  {
+    id: 1,
+    name: "Reciplease",
+    about: "A recipe tracking app",
+    technologies: ["Rails", "Bootstrap CSS"],
+  },
+  {
+    id: 2,
+    name: "Kibbles N Bitz",
+    about: "Tinder for dogs",
+    technologies: ["React", "Redux"],
+  },
+  {
+    id: 3,
+    name: "Alienwares",
+    about: "Etsy for aliens",
+    technologies: ["React", "Redux", "Rails"],
+  },
+]
+
+function ProjectList() {
+  const projectCards = projects.map((userObj) => {
+    return <ProjectItem isObject = {userObj.technologies[1,2]} key={userObj.id} name ={userObj.name} about={userObj.about} technologies={userObj.technologies}/>
+  })
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projectCards}</div>
     </div>
   );
 }
